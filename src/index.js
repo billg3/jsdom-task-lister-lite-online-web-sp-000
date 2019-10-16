b09b50d2e4ca8801997ecfeb431e4e5a9f7436d7
Bill G <gbilly3@gmail.com>
2019-10-16 15:19:00 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -6,21 +6,17 @@ document.addEventListener("DOMContentLoaded", () => {
       event.preventDefault()
 
 //create
-
     ul = document.querySelector('#tasks')
-
     li = document.createElement('li');
     taskInput = document.querySelector('#new-task-description').value
-
-
-
+//
     li.textContent = taskInput;
     ul.appendChild(li);
-
-
-
-
+//
     document.querySelector('#create-task-form').reset();
+
+
+
 
 
   })
