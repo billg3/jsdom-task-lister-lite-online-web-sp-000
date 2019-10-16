9f952215af736eb3cf075aa0f2035398fdf0d739
Bill G <gbilly3@gmail.com>
2019-10-16 15:31:34 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -6,25 +6,25 @@ document.addEventListener("DOMContentLoaded", () => {
       event.preventDefault()
 //
  // const taskList = new TaskList();
-   
+
 //create
     ul = document.querySelector('#tasks')
     li = document.createElement('li');
     taskInput = document.querySelector('#new-task-description').value
 //
     li.textContent = taskInput;
-    ul.appendChild(li);
+    abc = ul.appendChild(li);
 //
     document.querySelector('#create-task-form').reset();
 //
 
-      ul.addEventListener("click", (e) => {
-        if (e.target.nodeName === "BUTTON") {
-          taskList.deleteTask(e.target.dataset.description);
-          renderApp();
-        }
-      });
-
+      // ul.addEventListener("click", (e) => {
+      //   if (e.target.nodeName === "BUTTON") {
+      //     taskList.deleteTask(e.target.dataset.description);
+      //     renderApp();
+      //   }
+      // });
+      //
 
 
   })
